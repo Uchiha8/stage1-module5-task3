e1993270f4af2f8b69f7cc065fd08ce901a2629f
[200~2110107@newuu.uz <2110107@newuu.uz>
2025-03-02 14:19:58 +0500

    implement collection operations: add divideBy method and complete filterEvenNumbers, toUpperCaseCollection, findMax, findMin, and sum methods

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -2,26 +2,39 @@ package com.epam.mjc;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
+    public Operation<Integer> divideBy(Integer divider) {
+        return list -> list.stream()
+                .map(num -> num / divider)
+                .collect(Collectors.toList());
+    }
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream()
+                .filter(num -> num % 2 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().max(Integer::compareTo);
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream()
+                .flatMap(List::stream)
+                .min(Integer::compareTo);
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream()
+                .reduce(0, Integer::sum);
     }
 }
---
+++ src/main/java/com/epam/mjc/Operation.java
@@ -0,0 +1,8 @@
+package com.epam.mjc;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface Operation<T> {
+    List<T> apply(List<T> arg);
+}
